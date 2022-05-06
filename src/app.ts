3464c9a40e3e4d0b8ac129c37caa92a502a8d158
Sergio Tavares <sergio.tavares@consensotec.com.br>
2022-05-05 22:46:20 -0300

    Create repository and initial config project

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+class App {
+    public express: express.Application
+
+    public constructor() {
+        this.express = express()
+        this.middlewares()
+        this.database()
+        this.routes()
+    }
+
+    private middlewares(): void {
+        this.express.use(express.json())
+        this.express.use(cors())
+    }
+
+    private database(): void {
+        mongoose.connect('mongodb+srv://lela:lela123@cluster0.xbmim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    }
+
+    private routes(): void {
+        this.express.get('/', (req, res) => {
+            return res.send('Hello World')
+        })
+    }
+}
+
+export default new App().express
